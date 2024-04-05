3f37e38a1f058c60a93949e1b28f1d69bf5ec592
jannen47 <jannen47@users.noreply.github.com>
2024-04-05 15:11:17 -0400

    Add files via upload

---
+++ out2xyz.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+import glob
+import os
+
+
+a=glob.glob('*.gjf.out')
+b=os.path.splitext(a[0])
+
+start="Unable to Open any file for archive entry."
+end="The archive entry for this job was punched."
+
+for filename in a:
+    n = filename.split('.')[0]
+    log = open('%s.gjf.out' %n,'r')
+    xyz = open('%s.xyz' %n,'w')
+    geo=''
+    count=0
+    for line in log.readlines():
+        if start in line:
+            count+=1
+        elif count>=1 and end not in line:
+            i=line.replace('\n', '')
+            geo+=i
+        elif count>=1 and end in line:
+            break
+        
+    #coord=repr(geo)
+    coord=geo
+    coord=coord.split("\\")
+    coord=coord[16:-10]
+    xyz.write("%s" %len(coord))
+    xyz.write('\n'+'\n')
+
+    for i in coord:
+        i=i.replace(' ', '')
+        i= i.replace(',', '  ')
+        xyz.write("%s\n" %i)
+    log.close()
+    xyz.close()
+
+
